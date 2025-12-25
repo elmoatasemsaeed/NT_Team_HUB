b531df41f0c9ee97d503f7911e976ca6ef20a107
elmoatasemsaeed <elmoatasem.saeed@nationaltechnology.net>
2025-12-25 18:55:46 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ملف الإعدادات العام - يتم تحميله مرة واحدة لكل الأجهزة
+const GLOBAL_GITHUB_CONFIG = {
+    repoPath: "elmoatasemsaeed.github.io/NT_Team_HUB/", // اسم المستخدم / اسم المستودع
+    filePath: "data.json",                // اسم الملف الذي تخزن فيه البيانات
+    branch: "main",                       // اسم الفرع (غالباً main أو master)
+    token: "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"      // التوكن الخاص بك
+};
