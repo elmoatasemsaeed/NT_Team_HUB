b45f888ce47a6b36603cdaa8040889f7334706bf
elmoatasemsaeed <elmoatasem.saeed@nationaltechnology.net>
2025-12-25 19:05:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const GLOBAL_GITHUB_CONFIG = {
     repoPath: "elmoatasemsaeed.github.io/NT_Team_HUB/", // اسم المستخدم / اسم المستودع
     filePath: "data.json",                // اسم الملف الذي تخزن فيه البيانات
     branch: "main",                       // اسم الفرع (غالباً main أو master)
-    token: "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"      // التوكن الخاص بك
+    token: "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"      // التوكن الخاص بك
 
 };
+
