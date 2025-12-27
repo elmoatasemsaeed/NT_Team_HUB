10b53eea3d2194e95ac2c830beebdba60a4b4688
elmoatasemsaeed <elmoatasem.saeed@nationaltechnology.net>
2025-12-27 18:02:40 +0200

    Update config.js
    solve login issue

--- config.js
+++ config.js
@@ -10,13 +10,9 @@ const githubConfig = {
 const USERS_KEY = 'team_hub_users';
 const STORAGE_KEY = 'rememberedUser';
 
-// متغيرات عامة سيتم تعبئتها من الـ API
 let fieldsConfig = [];
 let chartsConfig = [];
 let employees = [];
 let users = []; 
 let visibilityConfig = {};
 let currentUserRole = null;
-let activeCharts = [];
-let currentEditingFieldIndex = null;
-let currentSort = { column: null, direction: 'asc' };
